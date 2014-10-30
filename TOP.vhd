44a625e6f13a4b557e22934645bb4d8578bf7456
Sebastian Wong <sebastian.wong@hotmail.com>
2014-10-30 17:01:48 +0800

    Working version for Lab 3A
    Also included the testing of offset in blinky program

--- TOP.vhd
+++ TOP.vhd
@@ -102,23 +102,43 @@ type MEM_256x32 is array (0 to 255) of std_logic_vector (31 downto 0); -- 256 wo
 -- Instruction Memory
 ----------------------------------------------------------------
 constant INSTR_MEM : MEM_256x32 := (
-			x"3c090000", -- start : lui $t1, 0x0000 --immediate 16bites loaded into upper half of reg, dest reg t1 
-			x"35290001", -- 			ori $t1, 0x0001 # constant 1 --lower half
-			x"3c081003", -- 			lui $t0, 0x1003 # DIP pointer, for VHDL
-			x"8d0c0000", --			lw  $t4, 0($t0) 
-			x"3c081002", --			lui $t0, 0x1002 # LED pointer, for VHDL
-			x"3c0a0000", -- loop: 	lui $t2, 0x0000
-			x"354a0004", -- 			ori $t2, 0x0004 # delay counter (n). Change according to the clock			
-			
---			x"01495022", -- delay: 	sub $t2, $t2, $t1  --12cycles ;3 lines bottom
---			x"354a0004", -- 			ori $t2, 0x0004 # delay counter (n). Change according to the clock --3 delay instructions, Delay counter = 4 implies 12 instructions will be wasted
-			
-			x"01495022", -- delay: 	sub $t2, $t2, $t1 
-			x"0149582a", -- 			slt $t3, $t2, $t1
-			x"1160fffd", -- 			beq $t3, $zero, delay
-			x"ad0c0000", -- 			sw  $t4, 0($t0)	
-			x"01806027", --			nor $t4, $t4, $zero
-			x"08100005", -- 			j loop # infinite loop; n*3 (delay instructions) + 5 (non-delay instructions).
+
+
+
+			x"3c090000",
+			x"35290001",
+			x"3c081002",
+			x"3508ffff",
+			x"8d0c0001",
+			x"3c081002",
+			x"3c0a0000",
+			x"354a0004",
+			x"01495022",
+			x"0149582a",
+			x"1160fffd",
+			x"ad0c0000",
+			x"01806027",
+			x"08100006",
+
+
+
+--			x"3c090000", -- start : lui $t1, 0x0000 --immediate 16bites loaded into upper half of reg, dest reg t1 
+--			x"35290001", -- 			ori $t1, 0x0001 # constant 1 --lower half
+--			x"3c081003", -- 			lui $t0, 0x1003 # DIP pointer, for VHDL
+--			x"8d0c0000", --			lw  $t4, 0($t0) 
+--			x"3c081002", --			lui $t0, 0x1002 # LED pointer, for VHDL
+--			x"3c0a0000", -- loop: 	lui $t2, 0x0000
+--			x"354a0004", -- 			ori $t2, 0x0004 # delay counter (n). Change according to the clock			
+--			
+----			x"01495022", -- delay: 	sub $t2, $t2, $t1  --12cycles ;3 lines bottom
+----			x"354a0004", -- 			ori $t2, 0x0004 # delay counter (n). Change according to the clock --3 delay instructions, Delay counter = 4 implies 12 instructions will be wasted
+--				
+--			x"01495022", -- delay: 	sub $t2, $t2, $t1 
+--			x"0149582a", -- 			slt $t3, $t2, $t1
+--			x"1160fffd", -- 			beq $t3, $zero, delay
+--			x"ad0c0000", -- 			sw  $t4, 0($t0)	
+--			x"01806027", --			nor $t4, $t4, $zero
+--			x"08100005", -- 			j loop # infinite loop; n*3 (delay instructions) + 5 (non-delay instructions).
 			others=> x"00000000");
 
 -- The Blinky program reads the DIP switches in the begining. Let the value read be VAL
